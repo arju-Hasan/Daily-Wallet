f9d94749281e8bb5e36a1fa6f6fc9909079a89bc
Arju-Hasan <rajuahmeed698@gmail.com>
2025-08-23 03:49:35 +0600

    add a home page

--- js/index.js
+++ js/index.js
@@ -1,1 +1,24 @@
-console.log("Raju Ahmed");
+// const form = document.getElementById("loginForm");
+// const mobile = document.getElementById("mobile");
+// const pin = document.getElementById("pin");
+// const loginBtn = document.getElementById("loginBtn");
+// 
+// form.addEventListener("submit", function (e) {
+//   if (mobile.value.trim() === "" || pin.value.trim() === "") {
+//     e.preventDefault();
+//     alert("Please enter Mobile Number and 4 Digit Pin!");
+//   } 
+// });
+ 
+
+ function loginFunction() {
+   const mobile = document.getElementById("mobile").value.trim();
+   const pin = document.getElementById("pin").value.trim();
+
+   if (mobile === "" || pin === "") {
+     alert("Please enter Mobile Number and 4 Digit Pin!");
+   } else {
+   
+     window.location.href = "home.html";
+   }
+ }
